11d95dcff402dca9ddec9c9728a3ab132605f930
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-02-04 16:24:53 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+
+let multiplo:boolean=false;
+
+function esMultiplo(numero1:number,numero2:number):boolean{
+  
+let division=numero1%numero2;
+
+if (division===0) {
+   multiplo=true;
+console.log(multiplo);
+ } else {
+   multiplo=false;
+   console.log(multiplo);
+ } 
+
+ return
+
+ 
+
+ }
+ 
+  
+  
+
+  
+
+
+let valordecalculoesmultiplo=esMultiplo(4,2);
+
+
+ 
+
+ 
